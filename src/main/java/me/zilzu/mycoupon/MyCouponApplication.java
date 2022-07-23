a60b4630b1c54f4310e443ed2afa51b8f9887bdd
zilzu <zilzu4165@naver.com>
2022-07-23 19:22:31 +0900

    init

---
+++ src/main/java/me/zilzu/mycoupon/MyCouponApplication.java
@@ -0,0 +1,13 @@
+package me.zilzu.mycoupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyCouponApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/zilzu/mycoupon/MyCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package me.zilzu.mycoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
