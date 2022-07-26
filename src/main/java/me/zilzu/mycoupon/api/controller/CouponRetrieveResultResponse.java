fa43976a62fcf72e151ff1cd6a9ffc8c31c111c9
zilzu <zilzu4165@naver.com>
2022-07-26 22:54:31 +0900

    retrieve coupon v4

--- src/main/java/me/zilzu/mycoupon/api/controller/CouponRetrieveResultResponse.java
+++ src/main/java/me/zilzu/mycoupon/api/controller/CouponRetrieveResultResponse.java
@@ -5,9 +5,48 @@ import me.zilzu.mycoupon.application.service.Coupon;
 
 public class CouponRetrieveResultResponse {
 
-    public Coupon coupon;
+    public String id;
+    public String object;
+    public Integer amountOff;
+    public long created;
+    public String currency;
+    public String duration;
+    public Integer durationInMonths;
+    public Boolean livemode;
+    public Integer maxRedemptions;
+    public String name;
+    public Float percentOff;
+    public Boolean valid;
+
+
+//    public CouponRetrieveResultResponse(String id, String object, Integer amountOff, long created, String currency, String duration, Integer durationInMonths, Boolean livemode, Integer maxRedemptions, String name, Float percentOff, Boolean valid) {
+//        this.id = id;
+//        this.object = object;
+//        this.amountOff = amountOff;
+//        this.created = created;
+//        this.currency = currency;
+//        this.duration = duration;
+//        this.durationInMonths = durationInMonths;
+//        this.livemode = livemode;
+//        this.maxRedemptions = maxRedemptions;
+//        this.name = name;
+//        this.percentOff = percentOff;
+//        this.valid = valid;
+//    }
 
     public CouponRetrieveResultResponse(Coupon coupon) {
-        this.coupon = coupon;
+        this.id = coupon.id;
+        this.object = coupon.object;
+        this.amountOff = coupon.amountOff;
+        this.created = coupon.created;
+        this.currency = coupon.currency;
+        this.duration = coupon.duration;
+        this.durationInMonths = coupon.durationInMonths;
+        this.livemode = coupon.livemode;
+        this.maxRedemptions = coupon.maxRedemptions;
+        this.name = coupon.name;
+        this.percentOff = coupon.percentOff;
+        this.valid = coupon.valid;
     }
+
 }
