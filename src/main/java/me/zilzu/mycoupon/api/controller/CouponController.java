35313031e0c65cfd3283d77f45248cfdf7f87b19
zilzu <zilzu4165@naver.com>
2022-07-27 23:06:54 +0900

    retrieveList coupons v1

--- src/main/java/me/zilzu/mycoupon/api/controller/CouponController.java
+++ src/main/java/me/zilzu/mycoupon/api/controller/CouponController.java
@@ -2,9 +2,17 @@ package me.zilzu.mycoupon.api.controller;
 
 import me.zilzu.mycoupon.application.service.Coupon;
 import me.zilzu.mycoupon.application.service.CouponService;
+import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class CouponController {
@@ -21,6 +29,25 @@ public class CouponController {
         Coupon coupon = couponService.retrieve(id);
 
         return new CouponRetrieveResultResponse(coupon);
+    }
+
+    @GetMapping("api/v1/coupons")
+    public CouponRetrieveListResponse retrieveListCoupons(HttpServletRequest request, @RequestBody Integer limit) {
+
+        String requestURI = request.getRequestURI();
+
+        List<Coupon> coupons = couponService.retrieveList(limit);
+
+        String objectType = "";
+        if (coupons.getClass().getName().equals("java.util.ArrayList")) {
+            objectType = "list";
+        }
+
+        List<CouponRetrieveResultResponse> couponListResults = coupons
+                .stream()
+                .map(CouponRetrieveResultResponse::new)
+                .collect(Collectors.toList());
+        return new CouponRetrieveListResponse(objectType, requestURI, false, couponListResults);
     }
 
 }
--- src/main/java/me/zilzu/mycoupon/application/service/CouponService.java
+++ src/main/java/me/zilzu/mycoupon/application/service/CouponService.java
@@ -2,13 +2,28 @@ package me.zilzu.mycoupon.application.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class CouponService {
 
     public Coupon retrieve(String id) {
-        return new Coupon(id,"coupon",null,System.currentTimeMillis()/1000,"usd",
-                "repeating",3, false,
-                null,"25.5% off",25.5F,true);
+        return new Coupon(id, "coupon", null, System.currentTimeMillis() / 1000, "usd",
+                "repeating", 3, false,
+                null, "25.5% off", 25.5F, true);
     }
 
+    public List<Coupon> retrieveList(Integer limit) {
+
+        List<Coupon> coupons = new ArrayList<>();
+
+        for (int i = 0; i < limit; i++) {
+            coupons.add(new Coupon("Z4OV52SU", "coupon", null, System.currentTimeMillis() / 1000, "usd",
+                    "repeating", 3, false,
+                    null, "25.5% off", 25.5F, true));
+        }
+
+        return coupons;
+    }
 }
