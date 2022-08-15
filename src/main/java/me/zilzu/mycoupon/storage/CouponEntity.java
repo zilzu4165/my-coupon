b2cada25551105dc33252eb43c2408d7c349e38d
zilzu <zilzu4165@naver.com>
2022-08-16 00:43:36 +0900

    feat : DB에 직접 저장될 CouponEntity 클래스 도입

---
+++ src/main/java/me/zilzu/mycoupon/storage/CouponEntity.java
@@ -0,0 +1,26 @@
+package me.zilzu.mycoupon.storage;
+
+
+import me.zilzu.mycoupon.common.enums.CouponCurrency;
+
+import java.time.LocalDateTime;
+
+public class CouponEntity {
+    public final String id;
+    public final String duration;
+    public final CouponCurrency couponCurrency;
+
+    public final LocalDateTime createdTime;
+
+
+    public LocalDateTime getCreatedTime() {
+        return createdTime;
+    }
+
+    public CouponEntity(String id, String duration, CouponCurrency couponCurrency, LocalDateTime createdTime) {
+        this.id = id;
+        this.duration = duration;
+        this.couponCurrency = couponCurrency;
+        this.createdTime = createdTime;
+    }
+}
