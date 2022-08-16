2996288c1f888c963bf017e78aaba3ff37304249
zilzu <zilzu4165@naver.com>
2022-08-16 22:04:42 +0900

    refactor : coupon Equals 재정의

--- src/main/java/me/zilzu/mycoupon/application/service/Coupon.java
+++ src/main/java/me/zilzu/mycoupon/application/service/Coupon.java
@@ -1,8 +1,9 @@
 package me.zilzu.mycoupon.application.service;
 
-import me.zilzu.mycoupon.common.enums.CouponCurrency;
+
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class Coupon {
     public final String id;
@@ -20,6 +21,19 @@ public class Coupon {
         this.duration = duration;
         this.couponCurrency = couponCurrency;
         this.createdTime = createdTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Coupon coupon = (Coupon) o;
+        return Objects.equals(id, coupon.id) && Objects.equals(duration, coupon.duration) && couponCurrency == coupon.couponCurrency && Objects.equals(createdTime, coupon.createdTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, duration, couponCurrency, createdTime);
     }
 
     @Override
