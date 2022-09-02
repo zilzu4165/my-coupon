c9985baa35691239f4c33f3fe7e05ff494daf758
zilzu <zilzu4165@naver.com>
2022-09-03 00:41:28 +0900

    test : ONCE 유형일 때 쿠폰 사용후 valid false

--- src/test/java/me/zilzu/mycoupon/application/service/CouponApplyTest.java
+++ src/test/java/me/zilzu/mycoupon/application/service/CouponApplyTest.java
@@ -26,4 +26,18 @@ public class CouponApplyTest {
         assertThat(apply).isEqualTo("사용됨");
     }
 
+    @DisplayName("duration이 ONCE 유형인 쿠폰은 사용후 valid가 false로 변경된다.")
+    @Test
+    void test2() {
+        CouponRequest couponRequest = new CouponRequest(CouponDuration.ONCE, null, DiscountType.AMOUNT, 1000L, null);
+        Coupon coupon = couponService.create(couponRequest);
+
+        couponService.apply(coupon.id);
+
+        Coupon foundCoupon = couponService.retrieve(coupon.id);
+
+        assertThat(foundCoupon.valid).isFalse();
+
+    }
+
 }
