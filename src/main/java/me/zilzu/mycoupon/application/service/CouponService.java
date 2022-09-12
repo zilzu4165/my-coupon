4e2ccdcc111c0e402bf77b225743b01a2475741f
zilzu <zilzu4165@naver.com>
2022-09-13 00:27:27 +0900

    refactor : 쿠폰 조회 -> entity.valid = false -> 쿠폰 put

--- src/main/java/me/zilzu/mycoupon/application/service/CouponService.java
+++ src/main/java/me/zilzu/mycoupon/application/service/CouponService.java
@@ -2,7 +2,6 @@ package me.zilzu.mycoupon.application.service;
 
 import me.zilzu.mycoupon.common.enums.CouponCurrency;
 import me.zilzu.mycoupon.common.enums.CouponDuration;
-import me.zilzu.mycoupon.common.enums.DiscountType;
 import me.zilzu.mycoupon.common.enums.SortingOrder;
 import me.zilzu.mycoupon.storage.CouponEntity;
 import me.zilzu.mycoupon.storage.CouponRepository;
@@ -19,11 +18,13 @@ public class CouponService {
     // autowired 는 테스트 코드 x
     private final CouponRepository couponRepository; // final : 변수에 값이 반드시 한번 할당이 되어야한다.
     private final CouponIdGenerate couponIdGenerate;
+    private final CouponValidator couponValidator;
 
     public CouponService(CouponRepository couponRepository,
-                         CouponIdGenerate couponIdGenerate) {
+                         CouponIdGenerate couponIdGenerate, CouponValidator couponValidator) {
         this.couponRepository = couponRepository;
         this.couponIdGenerate = couponIdGenerate;
+        this.couponValidator = couponValidator;
     }
 
     public Coupon retrieve(String id) {
@@ -47,33 +48,12 @@ public class CouponService {
     public Coupon createWithCurrency(CouponRequest couponRequest, CouponCurrency couponCurrency) {
         String couponId = couponIdGenerate.generate();
 
-        couponCreateValidate(couponRequest);
+        couponValidator.validate(couponRequest);
 
         CouponEntity entity = new CouponEntity(couponId, couponRequest.duration, couponRequest.durationInMonths, couponCurrency, couponRequest.discountType, couponRequest.amountOff, couponRequest.percentOff, true, LocalDateTime.now());
         couponRepository.save(entity);
 
         return new Coupon(entity.id, entity.duration, entity.durationInMonth, entity.couponCurrency, entity.discountType, entity.amountOff, entity.percentOff, entity.valid, entity.createdTime);
-    }
-
-    private static void couponCreateValidate(CouponRequest couponRequest) {
-        if (couponRequest.duration != CouponDuration.REPEATING && couponRequest.durationInMonths != null) {
-            throw new IllegalArgumentException("duration이 REPEATING 유형이 아니라면 durationInMonths 값을 가질 수 없습니다");
-        }
-        if (couponRequest.amountOff != null && couponRequest.percentOff != null) {
-            throw new IllegalArgumentException("금액할인과 비율할인이 동시에 값을 가질 수 없습니다.");
-        }
-        if (couponRequest.discountType == DiscountType.AMOUNT && couponRequest.amountOff == null) {
-            throw new IllegalArgumentException("discountType이 AMOUNT일 경우 amountOff 에 값이 존재해야 합니다.");
-        }
-        if (couponRequest.discountType == DiscountType.AMOUNT && couponRequest.percentOff != null) {
-            throw new IllegalArgumentException("discountType이 AMOUNT일 경우 percentOff 에 값이 존재할 수 없습니다.");
-        }
-        if (couponRequest.discountType == DiscountType.PERCENTAGE && couponRequest.percentOff == null) {
-            throw new IllegalArgumentException("discountType이 PERCENTAGE일 경우 percentOff 에 값이 존재해야 합니다.");
-        }
-        if (couponRequest.discountType == DiscountType.PERCENTAGE && couponRequest.amountOff != null) {
-            throw new IllegalArgumentException("discountType이 PERCENTAGE일 경우 amountOff 에 값이 존재할 수 없습니다.");
-        }
     }
 
     public CouponDeleteResult delete(String id) {
@@ -102,18 +82,15 @@ public class CouponService {
                 .collect(Collectors.toList());
     }
 
-    public String apply(String id) {
-        Coupon foundCoupon = retrieve(id);
+    public String apply(String couponId) {
+        Coupon foundCoupon = retrieve(couponId);
 
         if (!foundCoupon.valid) {
             throw new RuntimeException("사용할 수 없는 쿠폰입니다.");
         }
-        // CouponDuration이 ONCE 유형이면 사용후 valid false
-        if (foundCoupon.duration == CouponDuration.ONCE) {
-            CouponEntity entity = new CouponEntity(foundCoupon.id, foundCoupon.duration, foundCoupon.durationInMonth, foundCoupon.couponCurrency,
-                    foundCoupon.discountType, foundCoupon.amountOff, foundCoupon.percentOff, false, foundCoupon.createdTime);
 
-            couponRepository.applyCoupon(entity);
+        if (foundCoupon.duration == CouponDuration.ONCE) {
+            couponRepository.invalidate(foundCoupon.id);
         }
         return "사용됨";
     }
