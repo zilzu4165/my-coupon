f8adebcfde4fd8d544b8ef2ed4459f37f3122800
zilzu <zilzu4165@naver.com>
2022-09-25 01:32:25 +0900

    test : CouponValidatorTest 주석제거

--- src/test/java/me/zilzu/mycoupon/application/service/CouponValidatorTest.java
+++ src/test/java/me/zilzu/mycoupon/application/service/CouponValidatorTest.java
@@ -8,29 +8,8 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CouponValidatorTest {
-
     private final CouponValidator sut = new CouponValidator();
 
-    /**
-     * if (couponRequest.duration != CouponDuration.REPEATING && couponRequest.durationInMonths != null) {
-     * throw new IllegalArgumentException("duration이 REPEATING 유형이 아니라면 durationInMonths 값을 가질 수 없습니다");
-     * }
-     * if (couponRequest.amountOff != null && couponRequest.percentOff != null) {
-     * throw new IllegalArgumentException("금액할인과 비율할인이 동시에 값을 가질 수 없습니다.");
-     * }
-     * if (couponRequest.discountType == DiscountType.AMOUNT && couponRequest.amountOff == null) {
-     * throw new IllegalArgumentException("discountType이 AMOUNT일 경우 amountOff 에 값이 존재해야 합니다.");
-     * }
-     * if (couponRequest.discountType == DiscountType.AMOUNT && couponRequest.percentOff != null) {
-     * throw new IllegalArgumentException("discountType이 AMOUNT일 경우 percentOff 에 값이 존재할 수 없습니다.");
-     * }
-     * if (couponRequest.discountType == DiscountType.PERCENTAGE && couponRequest.percentOff == null) {
-     * throw new IllegalArgumentException("discountType이 PERCENTAGE일 경우 percentOff 에 값이 존재해야 합니다.");
-     * }
-     * if (couponRequest.discountType == DiscountType.PERCENTAGE && couponRequest.amountOff != null) {
-     * throw new IllegalArgumentException("discountType이 PERCENTAGE일 경우 amountOff 에 값이 존재할 수 없습니다.");
-     * }
-     */
 
     @Test
     @DisplayName("쿠폰 생성요청에서 할인 유형이 Amount(정량) 인데, 몇퍼센트를 할인해줄지 정보가 있으면 IllegalArgumentException 을 발생시킨다.")
