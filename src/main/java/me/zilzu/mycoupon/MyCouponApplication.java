7adce5d55094dd9a78790ba43cceaaa38852d7d0
songming05 <45953864+songming05@users.noreply.github.com>
2022-10-12 22:33:19 +0900

    Refactor: test 코드 수정 without thread sleep

--- src/main/java/me/zilzu/mycoupon/MyCouponApplication.java
+++ src/main/java/me/zilzu/mycoupon/MyCouponApplication.java
@@ -3,9 +3,11 @@ package me.zilzu.mycoupon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-@SpringBootApplication
+@EnableAspectJAutoProxy
 @EnableCaching
+@SpringBootApplication
 public class MyCouponApplication {
 
     public static void main(String[] args) {
