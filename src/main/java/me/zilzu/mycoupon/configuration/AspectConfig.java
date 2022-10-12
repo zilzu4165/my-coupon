26978a14c3f592d090a8268be9f4934993b78e3b
songming05 <45953864+songming05@users.noreply.github.com>
2022-10-12 22:33:19 +0900

    feat: aop repository 메서드 로깅

--- src/main/java/me/zilzu/mycoupon/configuration/AspectConfig.java
+++ src/main/java/me/zilzu/mycoupon/configuration/AspectConfig.java
@@ -1,16 +1,19 @@
 package me.zilzu.mycoupon.configuration;
 
+import me.zilzu.mycoupon.storage.CouponEntity;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
-@Component
 @Aspect
+@Component
 public class AspectConfig {
 
     //@Pointcut("within(me.zilzu.mycoupon.storage.NewCouponRepository)")
@@ -18,15 +21,27 @@ public class AspectConfig {
     public void repositoryMethod() {
     }
 
-    @Pointcut("execution(* me.zilzu.mycoupon.storage.*Repository.*(..)) || execution(* org.springframework.data.jpa.repository.JpaRepository.*(..))")
-    public void repositoryMethodAndDefaultJpa() {
-    }
-
-
     @Before(value = "repositoryMethod()")
     public void beforeMethod(JoinPoint joinPoint) {
 //        System.out.println("============ AOP Before ============");
 //        System.out.println("joinPoint = " + joinPoint);
+//        StringBuffer stringBuffer = new StringBuffer();s
+//        stringBuffer.append("============ AOP Before ============");
+//        stringBuffer.append("\n");
+//        stringBuffer.append("call at :: " + joinPoint.getSourceLocation());
+//        stringBuffer.append("\n");
+//        stringBuffer.append("MethodName [" + joinPoint.getSignature().getName() + "] :: " + joinPoint.getSignature());
+//        stringBuffer.append("\n");
+//        stringBuffer.append("arguments count :: " + joinPoint.getArgs().length);
+//        stringBuffer.append("\n");
+//        stringBuffer.append("arguments >> ");
+//        Arrays.stream(joinPoint.getArgs())
+//                .map(String::valueOf)
+//                .forEach(stringBuffer::append);
+//
+//        stringBuffer.append("\n");
+//        stringBuffer.append("============ AOP Before ============");
+//        System.out.println(stringBuffer);
     }
 
     @After(value = "repositoryMethod()")
@@ -35,13 +50,15 @@ public class AspectConfig {
 //        System.out.println("joinPoint = " + joinPoint);
     }
 
-    @Around(value = "repositoryMethodAndDefaultJpa()")
+    @Around(value = "repositoryMethod()")
     public Object aroundMethod(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
-        //Around_Before | Before | After | Around_After
-
+        //System.out.println("proceedingJoinPoint = " + proceedingJoinPoint);
 
         StringBuffer stringBuffer = new StringBuffer();
-        stringBuffer.append("============ AOP Around > Before ============\n");
+        stringBuffer.append("============ AOP Around > Before ============");
+        stringBuffer.append("\n");
+        stringBuffer.append("call at :: " + proceedingJoinPoint.getSourceLocation());
+        stringBuffer.append("\n");
         stringBuffer.append("MethodName [" + proceedingJoinPoint.getSignature().getName() + "] :: " + proceedingJoinPoint.getSignature());
         stringBuffer.append("\n");
         stringBuffer.append("arguments count :: " + proceedingJoinPoint.getArgs().length);
@@ -50,13 +67,16 @@ public class AspectConfig {
         Arrays.stream(proceedingJoinPoint.getArgs())
                 .map(String::valueOf)
                 .forEach(stringBuffer::append);
-        stringBuffer.append("\n");
 
-        // execute
+        stringBuffer.append("\n");
+        //System.out.println(stringBuffer);
+
         Object object = proceedingJoinPoint.proceed();
 
+//        System.out.println("object = " + object);
+        //stringBuffer = new StringBuffer();
         stringBuffer.append("============================================\n");
-        stringBuffer.append("DB results << " + object);
+        stringBuffer.append("object = " + object);
         stringBuffer.append("\n");
         if (object instanceof Optional) {
             if (((Optional<?>) object).isPresent()) {
@@ -65,14 +85,18 @@ public class AspectConfig {
                     stringBuffer.append("result count :: " + ((Collection<?>) o).size());
                     stringBuffer.append("\n");
                 }
+                if (o instanceof CouponEntity) {
+                    stringBuffer.append("DB result << " + o);
+                    stringBuffer.append("\n");
+                }
             }
         } else if (object instanceof Collection) {
             stringBuffer.append("result count :: " + ((Collection<?>) object).size());
             stringBuffer.append("\n");
         }
         stringBuffer.append("============ AOP Around > After ============");
-
         System.out.println(stringBuffer);
+
 
         return object;
     }
