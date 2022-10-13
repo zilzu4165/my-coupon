d7f53b03f9bf1d4589dd35647cb40de5c3128769
songming05 <45953864+songming05@users.noreply.github.com>
2022-10-13 22:31:55 +0900

    refactor: 예외핸들러 일괄적용 및 rename

--- src/main/java/me/zilzu/mycoupon/api/controller/CouponExceptionHandler.java
+++ src/main/java/me/zilzu/mycoupon/api/controller/CouponExceptionHandler.java
@@ -4,12 +4,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice(basePackages = {"me.zilzu.mycoupon.api"})
-public class CouponExceptionHandler {
+public class CouponExceptionHandler extends ResponseEntityExceptionHandler { // Exception Handling Consistency
 
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<CouponExceptionResponse> handleRuntimeException(RuntimeException exception) {
+    public ResponseEntity<CouponExceptionResponse> handleRuntimeException(RuntimeException runtimeException) {
         final String DEFAULT_ERROR = "INTERNAL_ERROR";
         final String DEFAULT_MESSAGE = "알 수 없는 서버 오류가 발생했습니다.";
 
@@ -18,12 +19,11 @@ public class CouponExceptionHandler {
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<CouponExceptionResponse> handleIllegalArgumentException(RuntimeException exception) {
+    public ResponseEntity<CouponExceptionResponse> handleIllegalArgumentException(IllegalArgumentException illegalArgumentException) {
         final String DEFAULT_ERROR = "INVALID_COUPON_CREATION_REQUEST";
         final String DEFAULT_MESSAGE = "쿠폰 생성 규칙에 알맞지 않습니다.";
 
         CouponExceptionResponse couponExceptionResponse = new CouponExceptionResponse(DEFAULT_ERROR, DEFAULT_MESSAGE);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(couponExceptionResponse);
     }
-
 }
