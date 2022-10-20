4c2c581775bae253ef0d316fa879d72aec5a91d8
zilzu <zilzu4165@naver.com>
2022-10-20 21:13:13 +0900

    refactor : CouponRequestDto 필드에 CouponCurrency 추가

--- src/main/java/me/zilzu/mycoupon/api/controller/CouponRequestDto.java
+++ src/main/java/me/zilzu/mycoupon/api/controller/CouponRequestDto.java
@@ -1,6 +1,7 @@
 package me.zilzu.mycoupon.api.controller;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import me.zilzu.mycoupon.common.enums.CouponCurrency;
 import me.zilzu.mycoupon.common.enums.CouponDuration;
 import me.zilzu.mycoupon.common.enums.DiscountType;
 
@@ -10,16 +11,18 @@ public class CouponRequestDto {
     public final Integer durationInMonths;
     @JsonProperty("discount_type")
     public final DiscountType discountType;
+    @JsonProperty("coupon_currency")
+    public final CouponCurrency couponCurrency;
     @JsonProperty("amount_off")
     public final Long amountOff;
     @JsonProperty("percent_off")
     public final Double percentOff;
 
-
-    public CouponRequestDto(CouponDuration duration, Integer durationInMonths, DiscountType discountType, Long amountOff, Double percentOff) {
+    public CouponRequestDto(CouponDuration duration, Integer durationInMonths, DiscountType discountType, CouponCurrency couponCurrency, Long amountOff, Double percentOff) {
         this.duration = duration;
         this.durationInMonths = durationInMonths;
         this.discountType = discountType;
+        this.couponCurrency = couponCurrency;
         this.amountOff = amountOff;
         this.percentOff = percentOff;
     }
--- src/main/java/me/zilzu/mycoupon/application/service/CouponCreationRequest.java
+++ src/main/java/me/zilzu/mycoupon/application/service/CouponCreationRequest.java
@@ -1,5 +1,6 @@
 package me.zilzu.mycoupon.application.service;
 
+import me.zilzu.mycoupon.common.enums.CouponCurrency;
 import me.zilzu.mycoupon.common.enums.CouponDuration;
 import me.zilzu.mycoupon.common.enums.DiscountType;
 
@@ -7,13 +8,15 @@ public class CouponCreationRequest {
     public final CouponDuration duration;
     public final Integer durationInMonths;
     public final DiscountType discountType;
+    public final CouponCurrency couponCurrency;
     public final Long amountOff;
     public final Double percentOff;
 
-    public CouponCreationRequest(CouponDuration duration, Integer durationInMonths, DiscountType discountType, Long amountOff, Double percentOff) {
+    public CouponCreationRequest(CouponDuration duration, Integer durationInMonths, DiscountType discountType, CouponCurrency couponCurrency, Long amountOff, Double percentOff) {
         this.duration = duration;
         this.durationInMonths = durationInMonths;
         this.discountType = discountType;
+        this.couponCurrency = couponCurrency;
         this.amountOff = amountOff;
         this.percentOff = percentOff;
     }
